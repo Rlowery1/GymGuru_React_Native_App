2b104b888ec77193048413044d319f3012c77599
Rlowery1 <69985002+Rlowery1@users.noreply.github.com>
2023-04-05 21:40:00 -0700

    Update aws-exports.sample.js

--- gymguru/aws-exports.sample.js
+++ gymguru/aws-exports.sample.js
@@ -5,9 +5,6 @@ const awsmobile = {
     "aws_cognito_region": "YOUR_AWS_COGNITO_REGION",
     "aws_user_pools_id": "YOUR_AWS_USER_POOLS_ID",
     "aws_user_pools_web_client_id": "YOUR_AWS_USER_POOLS_WEB_CLIENT_ID",
-    "aws_appsync_graphqlEndpoint": "YOUR_AWS_APPSYNC_GRAPHQLENDPOINT",
-    "aws_appsync_region": "YOUR_AWS_APPSYNC_REGION",
-    "aws_appsync_authenticationType": "YOUR_AWS_APPSYNC_AUTHENTICATION_TYPE",
 };
 
 export default awsmobile;
